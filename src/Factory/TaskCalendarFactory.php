4011722e901f4265b74f35bf87d4ceb96c0b5fc2
Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmail.com>
2024-02-06 00:03:38 +0100

    add factory

---
+++ src/Factory/TaskCalendarFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\TaskCalendar;
+use App\Repository\TaskCalendarRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<TaskCalendar>
+ *
+ * @method        TaskCalendar|Proxy                     create(array|callable $attributes = [])
+ * @method static TaskCalendar|Proxy                     createOne(array $attributes = [])
+ * @method static TaskCalendar|Proxy                     find(object|array|mixed $criteria)
+ * @method static TaskCalendar|Proxy                     findOrCreate(array $attributes)
+ * @method static TaskCalendar|Proxy                     first(string $sortedField = 'id')
+ * @method static TaskCalendar|Proxy                     last(string $sortedField = 'id')
+ * @method static TaskCalendar|Proxy                     random(array $attributes = [])
+ * @method static TaskCalendar|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static TaskCalendarRepository|RepositoryProxy repository()
+ * @method static TaskCalendar[]|Proxy[]                 all()
+ * @method static TaskCalendar[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static TaskCalendar[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static TaskCalendar[]|Proxy[]                 findBy(array $attributes)
+ * @method static TaskCalendar[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static TaskCalendar[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class TaskCalendarFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'Date' => self::faker()->dateTime(),
+            'isDone' => self::faker()->boolean(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(TaskCalendar $taskCalendar): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return TaskCalendar::class;
+    }
+}
