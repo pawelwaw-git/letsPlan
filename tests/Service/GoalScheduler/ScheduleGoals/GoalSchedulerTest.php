61f0ef27aec80892e940401b5aa14bcf9ee58bd8
Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmail.com>
2024-03-07 20:03:39 +0100

    tests for isScheduleAllowed function

--- tests/Service/GoalScheduler/ScheduleGoals/GoalSchedulerTest.php
+++ tests/Service/GoalScheduler/ScheduleGoals/GoalSchedulerTest.php
@@ -6,21 +6,27 @@ use App\Service\GoalScheduler\GoalScheduler;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class GoalSchedulerTest extends KernelTestCase
 {
-    private function mockRequest(): RequestStack
+    private function mockValidParamRequest(): RequestStack
     {
         $request = new Request(
-            [
-                GoalScheduler::QUERY_PARAMS => GoalScheduler::SCHEDULE_ACTION
-            ],
-            [],
-            [],
-            [],
-            [],
-            [],
+            [GoalScheduler::QUERY_PARAMS => GoalScheduler::SCHEDULE_ACTION],
+            [], [], [], [], [],
+            'response content'
+        );
+
+        $requestStack = new RequestStack();
+        $requestStack->push($request);
+
+        return $requestStack;
+    }
+
+    private function mockNotValidParamRequest(): RequestStack
+    {
+        $request = new Request(
+            [], [], [], [], [], [],
             'response content'
         );
 
@@ -37,26 +43,50 @@ class GoalSchedulerTest extends KernelTestCase
         self::bootKernel();
     }
 
-
     /**
      * @test
      */
-    public function isScheduleGoalsAllowed(): void
+    public function isScheduleGoalsAllowed_forRequestWithValidParam(): void
     {
         // GIVEN
-        $request_stack = $this->mockRequest();
+        $request_stack = $this->mockValidParamRequest();
 
         $container = static::getContainer();
 
-        $container->set(RequestStack::class, $request_stack);
+        $container->set('request_stack', $request_stack);
         /**
-         * @var GoalScheduler
+         * @var $goal_scheduler GoalScheduler
          */
         $goal_scheduler = $container->get(GoalScheduler::class);
 
         // WHEN
         $result = $goal_scheduler->isScheduleGoalsAllowed();
+
         // THEN
         $this->assertTrue($result);
+    }
+
+
+    /**
+     * @test
+     */
+    public function isScheduleGoalsNotAllowed_forRequestWithWrongParam(): void
+    {
+        // GIVEN
+        $request_stack = $this->mockNotValidParamRequest();
+
+        $container = static::getContainer();
+
+        $container->set('request_stack', $request_stack);
+        /**
+         * @var $goal_scheduler GoalScheduler
+         */
+        $goal_scheduler = $container->get(GoalScheduler::class);
+
+        // WHEN
+        $result = $goal_scheduler->isScheduleGoalsAllowed();
+
+        // THEN
+        $this->assertFalse($result);
     }
 }
