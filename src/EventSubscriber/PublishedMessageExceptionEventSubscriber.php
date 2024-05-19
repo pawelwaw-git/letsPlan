77eed680e79863c20dc93a5b824a9ee08dfe8e8e
Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmail.com>
2024-05-19 18:32:04 +0200

    change listener to subscriber

--- src/EventSubscriber/PublishedMessageExceptionEventSubscriber.php
+++ src/EventSubscriber/PublishedMessageExceptionEventSubscriber.php
@@ -2,19 +2,21 @@
 
 declare(strict_types=1);
 
-namespace App\EventListener;
+namespace App\EventSubscriber;
 
 use App\Exceptions\InvalidFilterException;
 use App\Exceptions\InvalidOperatorException;
 use Carbon\Exceptions\InvalidFormatException;
 use Doctrine\ORM\Query\QueryException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-class PublishedMessageExceptionListener
+class PublishedMessageExceptionEventSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event): void
     {
@@ -34,5 +36,12 @@ class PublishedMessageExceptionListener
         ];
 
         $event->setResponse(new JsonResponse($response_data, $code));
+    }
+    /**
+     * @return array<string, mixed>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::EXCEPTION => 'onKernelException'];
     }
 }
